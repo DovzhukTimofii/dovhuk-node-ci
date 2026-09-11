9685f2b862839b94afaa3142666893b186225336
DovzhukTimofii <121349911+DovzhukTimofii@users.noreply.github.com>
2026-09-11 13:40:31 +0300

    Add Terraform outputs

---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "instance_id" {
+  description = "ID of the created EC2 instance"
+  value       = aws_instance.nodeapp.id
+}
+
+output "instance_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.nodeapp.public_ip
+}
+
+output "application_url" {
+  description = "URL of the Node.js application"
+  value       = "http://${aws_instance.nodeapp.public_ip}:3000"
+}
+
+output "ssh_command" {
+  description = "Example SSH command"
+  value       = "ssh -i ${var.private_key_path} ubuntu@${aws_instance.nodeapp.public_ip}"
+}

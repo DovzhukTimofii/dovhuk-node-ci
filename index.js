8a1ff82afbb64228db9d86cf0b5609277f95de28
DovzhukTimofii <121349911+DovzhukTimofii@users.noreply.github.com>
2026-09-11 13:00:31 +0300

    Configure CI/CD deployment to AWS EC2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+const http = require("http");
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+  res.end("CI/CD deployment works!\n");
+});
+
+server.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on port ${port}`);
+});
